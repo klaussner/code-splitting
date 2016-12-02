e4350a9bf86f9fdde8dc40c2fc3376b8300f5ded
Christian Klaussner <developer@klaussner.me>
2016-12-02 15:00:14 +0100

    Bump version to 0.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'klaussner:code-splitting',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'Experimental code splitting build plugin for Meteor',
   git: 'https://github.com/klaussner/code-splitting'
 });
